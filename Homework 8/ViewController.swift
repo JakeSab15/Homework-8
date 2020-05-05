d009308d9dd376e46294ef14cca22e53c53a2ec9
Jakob Sabatula <j.sabatula@setonhill.edu>
2020-05-05 00:16:39 -0400

    Ready!

--- Homework 8/ViewController.swift
+++ Homework 8/ViewController.swift
@@ -9,6 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    var originalRect: CGRect!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBAction func foundTap(_ sender: Any) {
+        outputLabel.text = "Key 1 Unlocked!"
+    }
+    
+    @IBAction func slideMe(_ sender: Any) {
+        outputLabel.text = "Key 2 Unlocked!"
+    }
+    
+    @IBAction func rotation(_ sender: Any) {
+    var recognizer: UIRotationGestureRecognizer
+    var feedback: String
+    var rotation: CGFloat
+            
+    recognizer=sender as! UIRotationGestureRecognizer
+    rotation=recognizer.rotation
+    imageView.transform = CGAffineTransform(rotationAngle: 0.0)
+    outputLabel.text = "Unlocked, you can escape now!"
+    feedback=String(format: "Rotated, Redians: %1.2f, Velocity: %1.2f",
+        Float(recognizer.rotation),Float(recognizer.velocity))
+    imageView.transform = CGAffineTransform(rotationAngle: rotation)
+        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
